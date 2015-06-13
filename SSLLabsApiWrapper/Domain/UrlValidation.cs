47a6efa7c236be28ef66f9c9b4829606c9abbf6c
Michael Bisbjerg <michael@mbwarez.dk>
2015-06-13 15:52:58 +0200

    Cleanup

--- SSLLabsApiWrapper/Domain/UrlValidation.cs
+++ SSLLabsApiWrapper/Domain/UrlValidation.cs
@@ -2,29 +2,29 @@ using System;
 
 namespace SSLLabsApiWrapper.Domain
 {
-	class UrlValidation
-	{
-		public bool IsValid(string url)
-		{
-			bool valid = true;
+    class UrlValidation
+    {
+        public bool IsValid(string url)
+        {
+            bool valid = true;
 
-			Uri uri = null;
-			if (!Uri.TryCreate(url, UriKind.Absolute, out uri) || null == uri)
-			{
-				valid = false;
-			}
+            Uri uri = null;
+            if (!Uri.TryCreate(url, UriKind.Absolute, out uri) || null == uri)
+            {
+                valid = false;
+            }
 
-			return valid;
-		}
+            return valid;
+        }
 
-		public string Format(string url)
-		{
-			if (!url.EndsWith("/"))
-			{
-				url = url + "/";
-			}
+        public string Format(string url)
+        {
+            if (!url.EndsWith("/"))
+            {
+                url = url + "/";
+            }
 
-			return url;
-		}
-	}
+            return url;
+        }
+    }
 }
