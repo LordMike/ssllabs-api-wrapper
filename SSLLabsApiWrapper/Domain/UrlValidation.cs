06671abf61c2beb8c4f6c1ba2b14687f4d097e2c
Michael Bisbjerg <michael@mbwarez.dk>
2015-06-13 15:53:15 +0200

    Fix formatting of urls

--- SSLLabsApiWrapper/Domain/UrlValidation.cs
+++ SSLLabsApiWrapper/Domain/UrlValidation.cs
@@ -10,19 +10,15 @@ namespace SSLLabsApiWrapper.Domain
 
             Uri uri = null;
             if (!Uri.TryCreate(url, UriKind.Absolute, out uri) || null == uri)
-            {
                 valid = false;
-            }
 
             return valid;
         }
 
         public string Format(string url)
         {
-            if (!url.EndsWith("/"))
-            {
-                url = url + "/";
-            }
+            if (url.EndsWith("/"))
+                url = url.Substring(0, url.Length - 1);
 
             return url;
         }
